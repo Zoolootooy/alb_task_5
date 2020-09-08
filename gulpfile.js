d6d0992b2eb5ff123084902dbbc6c54fc13a4e56
Zoolootooy_dev <maxradjia.zp@gmail.com>
2020-09-08 18:10:44 +0300

    Added plans block

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 let project_folder = "dist";
 let source_folder = "#src";
 
+let fs = require('fs');
+
 let path = {
     build: {
         html: project_folder + "/",
         css: project_folder + "/css/",
         js: project_folder + "/js/",
         img: project_folder + "/img/",
+        fonts: project_folder + "/fonts/"
     },
 
     src: {
@@ -14,6 +17,7 @@ let path = {
         css: source_folder + "/scss/style.scss",
         js: source_folder + "/js/script.js",
         img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp,jpeg}",
+        fonts: source_folder + "/fonts/*.ttf"
     },
 
     watch: {
@@ -40,7 +44,9 @@ let {src, dest} = require('gulp'),
     imagemin = require('gulp-imagemin'),
     webp = require('gulp-webp'),
     webphtml = require('gulp-webp-html'),
-    webpcss = require('gulp-webpcss');
+    webpcss = require('gulp-webpcss'),
+    ttf2woff = require('gulp-ttf2woff'),
+    ttf2woff2 = require('gulp-ttf2woff2');
 
 function browserSync(params) {
     browsersync.init({
@@ -125,6 +131,38 @@ function images() {
         .pipe(browsersync.stream())
 }
 
+function fonts() {
+    src(path.src.fonts)
+        .pipe(ttf2woff())
+        .pipe(dest(path.build.fonts));
+    return src(path.src.fonts)
+        .pipe(ttf2woff2())
+        .pipe(dest(path.build.fonts));
+}
+
+function fontsStyle(params) {
+
+    let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+    if (file_content == '') {
+        fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+        return fs.readdir(path.build.fonts, function (err, items) {
+            if (items) {
+                let c_fontname;
+                for (var i = 0; i < items.length; i++) {
+                    let fontname = items[i].split('.');
+                    fontname = fontname[0];
+                    if (c_fontname != fontname) {
+                        fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                    }
+                    c_fontname = fontname;
+                }
+            }
+        })
+    }
+}
+
+function cb() { }
+
 function watchFiles(params) {
     gulp.watch([path.watch.html],html);
     gulp.watch([path.watch.css], css);
@@ -136,9 +174,11 @@ function clean(params) {
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.fontsStyle = fontsStyle;
+exports.fonts = fonts;
 exports.js = js;
 exports.images = images;
 exports.css = css;
